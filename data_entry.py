f6555663304cdc417494ea24a8fcc5c8e37f0997
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-10-04 13:39:12 +0545

    get_amount method

--- data_entry.py
+++ data_entry.py
@@ -1,19 +1,28 @@
 from datetime import datetime
 
+date_format = "%d-%m-%Y"
+
 def get_date(prompt, allow_default = False):
     date_str = input(prompt)
     if allow_default and not date_str:
-        return datetime.today().strftime("%d-%m-%Y")
+        return datetime.today().strftime(date_format)
     
     try:
-        valid_date = datetime.strptime(date_str,"%d-%m-%Y" )
-        return valid_date.strftime("%d-%m-%Y")
+        valid_date = datetime.strptime(date_str,date_format )
+        return valid_date.strftime(date_format)
     except ValueError:
         print("Invalid date format. Please enter date in dd-mm-yyyy format")
         return get_date(prompt, allow_default)
 
 def get_amount():
-    pass
+    try:
+        amount = float(input("Enter the amount: "))
+        if amount <= 0:
+            raise ValueError("Amount must be a non-negative non-zero value.")
+        return amount
+    except ValueError as e:
+        print(e)
+        return get_amount()
 
 def get_category():
     pass
