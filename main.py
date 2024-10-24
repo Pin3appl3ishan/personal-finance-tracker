06527f1112c05b98b6cfac427363d013179aa492
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-10-24 10:43:53 +0545

    plot promt to ask user

--- main.py
+++ main.py
@@ -110,6 +110,8 @@ def main():
             start_date = get_date("Enter the start date(dd-mm-yyyy): ")
             end_date = get_date("Enter the end date(dd-mm-yyyy): ")
             df = CSV.get_transactions(start_date, end_date)
+            if input("Do you want to see a plot? (y/n)").lower() == "y":
+                plot_transactoins(df)
         elif choice == "3":
             print("Exiting...")
             break
