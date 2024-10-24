3551abef225822b4b8bb1d942691517562ecc313
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-10-24 10:39:28 +0545

    Implement plot transaction with matplot

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import csv
 from datetime import datetime
 from data_entry import get_amount, get_category, get_date, get_description
+import matplotlib.pyplot as plt
 
 class CSV:
     CSV_FILE = "finance_data.csv"
@@ -69,6 +70,32 @@ def add():
     category = get_category()
     description = get_description()
     CSV.add_entry(date, amount, category, description)
+
+def plot_transactoins(df):
+    df.set_index("date", inplace=True)
+
+    income_df = (
+        df[df["category"] == "Income"] 
+        .resample("D")
+        .sum()
+        .reindex(df.index, fill_value = 0)
+    )
+    expense_df = (
+        df[df["category"] == "Expense"]
+        .resample("D")
+        .sum()
+        .reindex(df.index, fill_value = 0)
+    )
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(income_df.index, income_df["amount"], label="Income", color="g")
+    plt.plot(expense_df.index, expense_df["amount"], label="Expense", color="r")
+    plt.xlabel("Date")
+    plt.ylabel("Amount")
+    plt.title("Income and Expenses Over Time")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
 def main():
     while True:
